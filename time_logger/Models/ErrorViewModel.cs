db5e7d87445715c912f645dcff0b1f80eab14c8b
Simeon Kovachev <skcodingacc@gmail.com>
2023-11-19 11:49:32 +0200

    Add project files.

---
+++ time_logger/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace time_logger.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
